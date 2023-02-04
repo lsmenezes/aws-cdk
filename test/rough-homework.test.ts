1830e50c6582f4d6808c46a4d24de8725c6aa29a
Leonardo Menezes <leonardo@lsmenezes.com.br>
2023-02-04 13:34:38 -0300

    Initial commit

---
+++ test/rough-homework.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RoughHomework from '../lib/rough-homework-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/rough-homework-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RoughHomework.RoughHomeworkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
